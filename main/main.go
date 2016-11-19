5cac9b06f97cf7f51b9285ac59cfa720d47a144d
Jahn <ioannis.kiwe@gmail.com>
2016-11-20 00:20:44 +0100

    main/main.go: Got rid of unnecessary variables

--- main/main.go
+++ main/main.go
@@ -11,15 +11,11 @@ const INTERVAL = 500
 
 var NUM_ROWS_PTR = flag.Int("rows", 40, "The number of rows")
 var NUM_COLS_PTR = flag.Int("cols", 150, "The number of cols")
-var NUM_OF_INIT_SEEDS_PTR = flag.Int("seeds", 500, "The number of initial random seeds")
+var NUM_OF_SEEDS_PTR = flag.Int("seeds", 500, "The number of initial random seeds")
 
 func main() {
 	flag.Parse()
-	NUM_ROWS := *NUM_ROWS_PTR
-	NUM_COLS := *NUM_COLS_PTR
-	NUM_OF_INIT_SEEDS := *NUM_OF_INIT_SEEDS_PTR
-	config := utils.Config{NUM_ROWS, NUM_COLS, NUM_OF_INIT_SEEDS}
-
+	config := utils.Config{*NUM_ROWS_PTR, *NUM_COLS_PTR, *NUM_OF_SEEDS_PTR}
 	l := life.Create(config)
 	for {
 		time.Sleep(time.Millisecond * INTERVAL)
